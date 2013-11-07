02693b41fee639be020adf464b5b41a04b0343d4
Dan Brown <dan.brown180@gmail.com>
2013-11-07 10:38:31 +0000

    initial

---
+++ facebook-canvas.js
@@ -0,0 +1,42 @@
+function PostImageToFacebook(authToken)
+{
+var canvas = document.getElementById("c");
+var imageData  = canvas.toDataURL("image/png");
+try{
+    blob = dataURItoBlob(imageData);
+}catch(e){console.log(e);}
+var fd = new FormData();
+fd.append("access_token",authToken);
+fd.append("source", blob);
+fd.append("message","Photo Text");
+try{
+$.ajax({
+    url:"https://graph.facebook.com/me/photos?access_token=" + authToken,
+    type:"POST",
+    data:fd,
+    processData:false,
+    contentType:false,
+    cache:false,
+    success:function(data){
+        console.log("success " + data);
+    },
+    error:function(shr,status,data){
+        console.log("error " + data + " Status " + shr.status);
+    },
+    complete:function(){
+    console.log("Posted to facebook");
+    }
+});
+
+}catch(e){console.log(e);}
+}
+
+function dataURItoBlob(dataURI) {
+var byteString = atob(dataURI.split(',')[1]);
+var ab = new ArrayBuffer(byteString.length);
+var ia = new Uint8Array(ab);
+for (var i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i);
+}
+return new Blob([ab], { type: 'image/png' });
+}
